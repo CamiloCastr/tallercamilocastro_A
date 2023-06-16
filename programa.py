caf0d21113bd907be9925bbee00ea9e1e8d4ecd9
CamiloCastr <135990381+CamiloCastr@users.noreply.github.com>
2023-06-15 22:03:33 -0500

    Add files via upload

---
+++ programa.py
@@ -0,0 +1,7 @@
+from Matematicas import sumar,restar,multiplicar, dividir 
+from algebra import distancia 
+print(sumar(4,4))
+print (restar(8,6))
+print(multiplicar (5,3))
+print(dividir(15,3))
+print (distancia(1,2,3,4))
